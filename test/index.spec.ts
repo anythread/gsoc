676903f8096a609cc1e5789f13f4e8a15890b40d
Viktor Levente Tóth <toth.viktor.levente@gmail.com>
2024-10-01 16:39:26 +0200

    test: gsoc arrive

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -47,6 +47,8 @@ describe('gsoc', () => {
     await gsoc2.send('message 6', resourceId)
     await gsoc.send('message 7', resourceId)
 
+    await waitGsocArrive(1000)
+
     close()
 
     expect(messages).toEqual([
@@ -87,16 +89,16 @@ describe('gsoc', () => {
       gsoc.send('message 7', resourceId),
     ])
 
+    await waitGsocArrive(1000)
+
     close()
 
-    expect(messages.sort()).toEqual([
-      'message 1',
-      'message 2',
-      'message 3',
-      'message 4',
-      'message 5',
-      'message 6',
-      'message 7',
-    ])
+    // two different postage batches with two updates (new GSOC postage timestamp can be smaller than the previous GSOC's timestamp)
+    expect(messages.length).toBeGreaterThanOrEqual(4)
   })
 })
+
+// awaits for the gsoc message to be synced on test node
+async function waitGsocArrive(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
